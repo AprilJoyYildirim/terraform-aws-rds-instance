96484d0d1936af7714e3279b694f20037fbaf49b
EC2 Default User <ec2-user@ip-172-31-21-76.eu-west-2.compute.internal>
2021-05-20 01:47:33 +0000

    added module

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "subnet_ids" {
+  type = list(any)
+}
+variable "identifier" {}
+variable "storage_type" {}
+variable "allocated_storage" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "name" {}
+variable "username" {}
+variable "password" {}
+variable "publicly_accessible" {}
+variable "allowed_hosts" {
+  type = list(any)
+}
